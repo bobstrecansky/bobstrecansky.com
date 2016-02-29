8361967e84a678e5a6da95b71d73927a0cc594e1
root <root@bobstrecansky.com>
2016-02-29 00:29:50 +0000

    Initial Configuration

---
+++ README.md
@@ -0,0 +1,1 @@
+Interactive, Responsive Design Resume
---
+++ checklogin.php
@@ -0,0 +1,51 @@
+<?php
+	
+	ob_start();
+	session_start();
+	include_once 'config.php';
+
+	// Connect to server and select databse.
+	try
+	{
+		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+		$db = new PDO('mysql:host='.$host.';dbname='.$db_name.';charset=utf8', $username, $password);
+	}
+	catch(Exception $e)
+	{
+		die('Error : ' . $e->getMessage());
+	}	
+
+	// Define $myusername and $mypassword 
+	$myusername = $_POST['myusername']; 
+	$mypassword = $_POST['mypassword']; 
+
+	// To protect MySQL injection
+	$myusername = stripslashes($myusername);
+	$mypassword = stripslashes($mypassword);
+		
+	$stmt = $db->query("SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'");
+
+	// rowCount() is counting table row
+	$count = $stmt->rowCount();
+
+	// If result matched $myusername and $mypassword, table row must be 1 row
+	if($count == 1){
+
+		// Register $myusername, $mypassword and print "true"
+		$redirect_page = '/';
+		header('Location: '.$redirect_page);
+		setcookie('user', $myusername);
+		$_SESSION['username'] = 'myusername';
+		$_SESSION['password'] = 'mypassword';
+		header('Location: '.$redirect_page);
+        setcookie('user', $myusername);
+
+		
+	}
+	else {
+		//return the error message
+		echo "<div class=\"alert alert-danger alert-dismissable\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>Wrong Username or Password</div>";
+	}
+
+	ob_end_flush();
+?>
